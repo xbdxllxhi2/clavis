1b1550fff0a29215fd87ca5b99ca341de74d954e
xbdxllxhi2 <abdallahi2002.aa@gmail.com>
2024-07-06 16:59:37 +0100

    feat: enchance kafka producer SPI

--- kcspi/src/main/java/xyz/clavis/kcspi/KCUserCreationEventProvider.java
+++ kcspi/src/main/java/xyz/clavis/kcspi/KCUserCreationEventProvider.java
@@ -1,5 +1,6 @@
 package xyz.clavis.kcspi;
 
+import java.io.IOException;
 import java.util.Properties;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -9,13 +10,14 @@ import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.admin.AdminEvent;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.util.JsonSerialization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class KCUserCreationEventProvider implements EventListenerProvider {
   private KeycloakSession keycloakSession;
-  private KafkaProducer<String, String> producer;
+  private KafkaProducer<String, Object> producer;
   private final String topic;
 
   Logger logger = LoggerFactory.getLogger(KCUserCreationEventProvider.class);
@@ -26,13 +28,11 @@ public class KCUserCreationEventProvider implements EventListenerProvider {
     this.keycloakSession = keycloakSession;
 
     Properties properties = new Properties();
-    properties
-        .setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootsrapServers);
-    properties
-        .setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-    properties
-        .setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-            StringSerializer.class.getName());
+    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootsrapServers);
+    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+        StringSerializer.class.getName());
+    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+        StringSerializer.class.getName());
 
     this.producer = new KafkaProducer<>(properties);
     this.topic = topicName;
@@ -42,24 +42,28 @@ public class KCUserCreationEventProvider implements EventListenerProvider {
   @Override
   public void onEvent(Event event) {
     logger.info("Yo i got this Event: {} ,{} ,{} ,{} ,{} ", event.getDetails(),
-        event.getIpAddress(),
-        event.getClientId(), event.getType(), event.getError());
+        event.getIpAddress(), event.getClientId(), event.getType(), event.getError());
 
-    this.producer.send(new ProducerRecord<>(topic, "Event published to kafka"));
+    this.producer.send(new ProducerRecord<>(topic, event));
+
   }
 
   @Override
   public void onEvent(AdminEvent event, boolean b) {
     logger.info("Yo i got this Event: {} ,{} ,{} ,{} ,{} ,{} ", event.getResourceTypeAsString(),
-        event.getAuthDetails(),
-        event.getRepresentation(), event.getOperationType(), event.getError(),
-        event.getResourceType());
+        event.getAuthDetails(), event.getRepresentation(), event.getOperationType(),
+        event.getError(), event.getResourceType());
+    event.getResourceType();
+    try {
+      this.producer.send(new ProducerRecord<>(topic, JsonSerialization.writeValueAsString(event)));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
 
-    this.producer.send(new ProducerRecord<>(topic, "Admin event published to kafka"));
   }
 
   @Override
   public void close() {
-
+    this.producer.close();
   }
 }
